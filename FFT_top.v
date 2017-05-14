52e252a044ecac45ec14188b9418d2e509eb424a
qingzengsong <qingzengsong@163.com>
2017-05-14 14:18:11 +0800

    Add files via upload

---
+++ FFT_top.v
@@ -0,0 +1,10 @@
+ix($compile): couple of unit tests for IE9
+
+Older IEs serialize html uppercased, but IE9 does not...
+Would be better to expect case insensitive, unfortunately jasmine does
+not allow to user regexps for throw expectations.
+
+Closes #392
+Breaks foo.bar api, foo.baz should be used instead
+
+module FFT_top();
